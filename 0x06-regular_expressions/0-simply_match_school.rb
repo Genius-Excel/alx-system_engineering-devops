aa0eeb555b3c02b1c663b626d843c77ba54eb0f9
Genius-Excel <geniusexcel9@gmail.com>
2024-01-04 15:18:47 -0800

    File modified.

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-
-def match_pattern(str)
-	regex = /School/
-	match_result = str.match(regex)
-
-	if match_result
-		puts match_result[0]
-	else
-		puts "$"
-	end
-end
-
-if ARGV.empty?
-	puts "Usage: #{$PROGRAM_NAME} {STRING}"
-	exit 1
-end
-
-match_pattern(ARGV[0])	
+# This script checks the pattern fo Shool
+puts ARGV[0].scan(/School/).join
