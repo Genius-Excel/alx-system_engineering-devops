7180d1e94f7d7620b06c51f1a20a3b319aead24f
Genius-Excel <geniusexcel9@gmail.com>
2024-04-11 07:39:30 -0700

    File modified to meet new requiremnets.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
-"""This module contains a function that Reddit API"""
+"""This module contains a function that Reddit API
+   the function queries the Reddit API for subscribers.
+"""
 
 
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """This function makaes a GET HTTP method to Reddit API
+    """
+
     url = f"https://www.reddit.com/r/{subreddit}/about"
     headers = {'User-Agent': 'Custom User Agent'}
 
