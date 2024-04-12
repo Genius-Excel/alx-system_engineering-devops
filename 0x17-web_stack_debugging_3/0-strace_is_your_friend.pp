784daacd08f2b804a111466bd7c35e4c409a4e98
Genius-Excel <geniusexcel9@gmail.com>
2024-04-12 15:48:53 -0700

    New changes made to pass requirments.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,11 @@
 # Install strace package if not already installed
+
 package { 'strace':
   ensure => installed,
 }
 
 # Run strace on Apache process to trace system calls
+
 exec { 'strace_apache':
   command => 'strace -f -s 1024 -o /tmp/apache_strace.txt -p $(pidof apache2)',
   user    => 'root',
@@ -13,6 +15,7 @@ exec { 'strace_apache':
 }
 
 # Fix the identified issue (replace this with your actual fix)
+
 exec { 'fix_issue':
   command => 'echo "Fixing the issue"',
   user    => 'root',
@@ -21,4 +24,3 @@ exec { 'fix_issue':
   onlyif  => 'grep -q "error message" /tmp/apache_strace.txt',
   require => Exec['strace_apache'],
 }
-
