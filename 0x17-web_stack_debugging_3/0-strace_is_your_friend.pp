2beb018fc25f38e4e12436ade56881714db5235c
Genius-Excel <geniusexcel9@gmail.com>
2024-04-16 03:36:59 -0700

    Strace used to log errors.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,26 +1,6 @@
-# Install strace package if not already installed
+# print the server error to standard output.
 
-package { 'strace':
-  ensure => installed,
-}
-
-# Run strace on Apache process to trace system calls
-
-exec { 'strace_apache':
-  command => 'strace -f -s 1024 -o /tmp/apache_strace.txt -p $(pidof apache2)',
-  user    => 'root',
-  group   => 'root',
-  path    => '/usr/bin',
-  require => Package['strace'],
-}
-
-# Fix the identified issue (replace this with your actual fix)
-
-exec { 'fix_issue':
-  command => 'echo "Fixing the issue"',
-  user    => 'root',
-  group   => 'root',
-  path    => '/usr/bin',
-  onlyif  => 'grep -q "error message" /tmp/apache_strace.txt',
-  require => Exec['strace_apache'],
+exec { 'log_error':
+  command => 'strace -p $(pidof apache2) 2>&1 | grep open',
+  path => '/usr/bin'
 }
