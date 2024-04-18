6aa72b9a3f8d3dabefa7bbc54cbf890b4dd00a5a
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 12:40:31 -0700

    I don tire for this task.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix Ngnix error.
 
 exec { 'fix--for-ngnx':
-  command => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix/; sudo ngnix service restart',
-  path    => '/bin/'
+  command   => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix/; sudo ngnix service restart',
+  provider  => shell,
 }
