045bcbae03def95dab1059e746624eec08e32d47
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 12:44:31 -0700

    File modified to check if requirment would be met.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# fix Ngnix error.
+# fix Ngnix error to avoid failed requests.
 
 exec { 'fix--for-ngnx':
   command   => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix/; sudo ngnix service restart',
