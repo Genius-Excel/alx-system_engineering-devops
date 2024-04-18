1083a3b45c032731e976024bd5c41662819d9f34
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 13:14:53 -0700

    Just work pleaseeeeeeeee.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix Ngnix error to avoid failed requests.
 
 exec { 'change nginx limit':
-    command  => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix; sudo service nginx restart',
-    provider => shell,
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix; sudo service nginx restart',
+  provider => shell,
 }
