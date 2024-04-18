09ad22e7af371a5a859745ab979ac2b8e4bb2fef
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 12:53:08 -0700

    typo corrected, let's see if checker would pass.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix Ngnix error to avoid failed requests.
 
-exec { 'change ngnix limit':
+exec { 'change nginx limit':
     command  => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix/; sudo ngnix service restart',
     provider => shell,
 }
