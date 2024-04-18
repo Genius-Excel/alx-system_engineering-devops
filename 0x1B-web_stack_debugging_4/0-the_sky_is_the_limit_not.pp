34db88870d8e99839aeea2cb12a8614598189bb4
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 13:39:28 -0700

    Sky is not the limit I know, but can you pass the cheker please?

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # this puppet manifest is meant to resolve requests limit error
 
 exec { 'change-nginx-limit':
-  command  => 'sudo sed -i "s/15/4096/" /etc/default/ngnix',
-  provider => shell,
+  command => 'sudo sed -i "s/15/4096/" /etc/default/ngnix',
+  path    => 'usr/bin/local/:/bin/',
 }
 
 # Restart nginx after upadting the limit.
 -> exec { 'restart-ngnix':
-  command  => 'nginx restart',
-  provider => shell,
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
 }
