082fde85faef98e98dc7aafdc696f469f41d0b1b
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 13:11:24 -0700

    Puppet code modified.

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # Change OS configuration.
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 2048',
+exec { 'soft-os-configuration-for-holberton-user':
+  command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'hard-os-configuration-for-holberton-user':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
 }
