b0e10693bc212aea4ed216c86f2686ba59b4e359
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 12:46:05 -0700

    Puppet code modified.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fix Ngnix error to avoid failed requests.
 
-exec { 'fix--for-ngnx':
+exec { 'change ngnix limit':
   command   => 'sudo sed -i "s/15/4096/g" /etc/default/ngnix/; sudo ngnix service restart',
   provider  => shell,
 }
