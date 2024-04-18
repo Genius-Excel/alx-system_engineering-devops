2226846844bfaae0d232f0f3a2555e37d82bc5bc
Genius-Excel <geniusexcel9@gmail.com>
2024-04-18 13:42:10 -0700

    countless trials.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # this puppet manifest is meant to resolve requests limit error
 
-exec { 'change-nginx-limit':
+exec { 'fix--for-nginx':
   command => 'sudo sed -i "s/15/4096/" /etc/default/ngnix',
-  path    => 'usr/bin/local/:/bin/',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Restart nginx after upadting the limit.
--> exec { 'restart-ngnix':
+-> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/',
 }
